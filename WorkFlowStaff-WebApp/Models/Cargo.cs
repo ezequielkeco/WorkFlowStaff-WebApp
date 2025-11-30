4164f00e2e5cc41e19a438c90046b2390b625966
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2025-11-30 19:41:50 -0400

    Modelo Cargo
    Creación del modelo de datos para Cargo

---
+++ WorkFlowStaff-WebApp/Models/Cargo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace WorkFlowStaff_WebApp.Models
+{
+    public class Cargo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del cargo es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Nombre del Cargo")]
+        public string Nombre { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "Responsabilidades")]
+        public string Responsabilidades { get; set; }
+
+        public ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+    }
+}
