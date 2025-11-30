e074a3393af54ef616247fab009614c22313c097
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2025-11-30 19:54:11 -0400

    Contexto de datos
    Creación de la clase ApplicationDbContext para el contexto de datos

---
+++ WorkFlowStaff-WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkFlowStaff_WebApp.Models;
+
+namespace WorkFlowStaff_WebApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Empleado> Empleados { get; set; } = default!;
+        public DbSet<Departamento> Departamentos { get; set; } = default!;
+        public DbSet<Cargo> Cargos { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+    }
+}
