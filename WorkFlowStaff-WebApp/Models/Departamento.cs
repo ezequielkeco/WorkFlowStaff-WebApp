561132b9e09d233929541d3d629c3b9ca9314c5b
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2025-11-30 19:38:48 -0400

    Modelo Departamento
    Creación del modelo de datos para Departamento

---
+++ WorkFlowStaff-WebApp/Models/Departamento.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace WorkFlowStaff_WebApp.Models
+{
+    public class Departamento
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre del departamento es obligatorio.")]
+        [StringLength(100)]
+        [Display(Name = "Nombre del Departamento")]
+        public string Nombre { get; set; }
+
+        [StringLength(500)]
+        [Display(Name = "Descripción")]
+        public string Descripcion { get; set; }
+
+        public ICollection<Empleado> Empleados { get; set; } = new List<Empleado>();
+    }
+}
