e7870379936a1b9c3a8f21639b7b2a82be5666fd
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2025-11-30 19:12:19 -0400

    Primeros commits

---
+++ WorkFlowStaff-WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WorkFlowStaff_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
