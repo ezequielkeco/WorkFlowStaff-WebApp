bd9ce65da0e87c5c311647be2db75455a7210ca7
Ezequiel Santos <87910910+ezequielkeco@users.noreply.github.com>
2025-12-04 17:47:39 -0400

    Cambios Globales
    Se realizaron cambios globales para mejorar funcionalidades y usabilidad.

--- WorkFlowStaff-WebApp/Models/Empleado.cs
+++ WorkFlowStaff-WebApp/Models/Empleado.cs
@@ -29,7 +29,7 @@ namespace WorkFlowStaff_WebApp.Models
 
         [Required(ErrorMessage = "El salario es obligatorio.")]
         [Column(TypeName = "decimal(18, 2)")]
-        [Display(Name = "Salario")]
+        [Display(Name = "Salario Bruto")]
         public decimal Salario { get; set; }
 
         [Display(Name = "Vigente")]
@@ -54,17 +54,67 @@ namespace WorkFlowStaff_WebApp.Models
 
         [NotMapped]
         [Display(Name = "AFP")]
-        public decimal AFP => Salario * 0.0287m;
+        public decimal AFP
+        {
+            get
+            {
+                return Salario * 0.0287m;
+            }
+        }
 
         [NotMapped]
         [Display(Name = "ARS")]
-        public decimal ARS => Salario * 0.0304m;
+        public decimal ARS
+        {
+            get
+            {
+                return Salario * 0.0304m; // 3.04%
+            }
+        }
 
         [NotMapped]
         [Display(Name = "ISR")]
-        public decimal ISR => (Salario > 40000) ? Salario * 0.10m : 0m;
+        public decimal ISR
+        {
+            get
+            {
+                decimal salarioAnual = Salario * 12m;
+
+                decimal tramo1 = 416220m;
+                decimal tramo2 = 624329m;
+                decimal tramo3 = 867123m;
+
+                decimal isrAnual = 0m;
+
+                if (salarioAnual <= tramo1)
+                {
+                    isrAnual = 0m;
+                }
+                else if (salarioAnual <= tramo2)
+                {
+                    isrAnual = (salarioAnual - tramo1) * 0.15m;
+                }
+                else if (salarioAnual <= tramo3)
+                {
+                    isrAnual = (tramo2 - tramo1) * 0.15m
+                             + (salarioAnual - tramo2) * 0.20m;
+                }
+                else
+                {
+                    isrAnual = (tramo2 - tramo1) * 0.15m
+                             + (tramo3 - tramo2) * 0.20m
+                             + (salarioAnual - tramo3) * 0.25m;
+                }
+
+                return isrAnual / 12m;
+            }
+        }
 
         public Departamento? Departamento { get; set; }
         public Cargo? Cargo { get; set; }
+
+        [NotMapped]
+        [Display(Name = "Salario Neto")]
+        public decimal NominaNeta => Salario - AFP - ARS - ISR;
     }
 }
